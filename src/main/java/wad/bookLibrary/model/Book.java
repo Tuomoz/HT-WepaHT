66f3edc5c5d79bcb8a7e654acdd83e4177260557
Tuomo Kärkkäinen <tuomon.email@gmail.com>
2013-10-23 16:21:07 +0300

    Model for books

---
+++ src/main/java/wad/bookLibrary/model/Book.java
@@ -0,0 +1,87 @@
+package wad.bookLibrary.model;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name = "Book")
+public class Book 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name = "id")
+    Long id;
+    
+    @Column(name = "name")
+    @NotBlank
+    String name;
+    
+    @Column(name = "ISBN")
+    @NotBlank
+    String isbn;
+    
+    @Column(name = "authors")
+    @ElementCollection
+    List<String> authors;
+    
+    @Column(name = "publishers")
+    @ElementCollection
+    List<String> publishers;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getIsbn()
+    {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn)
+    {
+        this.isbn = isbn;
+    }
+
+    public List<String> getAuthors()
+    {
+        return authors;
+    }
+
+    public void setAuthors(List<String> authors)
+    {
+        this.authors = authors;
+    }
+
+    public List<String> getPublishers()
+    {
+        return publishers;
+    }
+
+    public void setPublishers(List<String> publishers)
+    {
+        this.publishers = publishers;
+    }
+}

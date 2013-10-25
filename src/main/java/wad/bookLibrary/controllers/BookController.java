d089ee8d8af3a693d4d87728952434c31a3dadf0
Tuomo Kärkkäinen <tuomon.email@gmail.com>
2013-10-25 14:34:30 +0300

    Book controller

---
+++ src/main/java/wad/bookLibrary/controllers/BookController.java
@@ -0,0 +1,24 @@
+package wad.bookLibrary.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import wad.bookLibrary.models.Book;
+import wad.bookLibrary.services.BookService;
+
+@Controller
+public class BookController 
+{
+    @Autowired
+    private BookService bookService;
+    
+    @RequestMapping(value = "books", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Book> list()
+    {
+        return bookService.list();
+    }
+}

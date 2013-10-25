f7cbec36d327eefbfdc7717f9dcd693ff7af97fa
Tuomo Kärkkäinen <tuomon.email@gmail.com>
2013-10-25 14:35:18 +0300

    Init class to fill the database with some values

---
+++ src/main/java/wad/bookLibrary/init/InitService.java
@@ -0,0 +1,42 @@
+package wad.bookLibrary.init;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import wad.bookLibrary.models.Book;
+import wad.bookLibrary.repositories.BookRepository;
+
+@Component
+public class InitService 
+{
+    @Autowired
+    BookRepository bookRepo;
+    
+    @PostConstruct
+    private void init()
+    {
+        Book book = new Book();
+        List<String> authors = new ArrayList<String>();
+        List<String> pubs = new ArrayList<String>();
+        
+        authors.add("Auth 1");
+        pubs.add("Pub 1");
+        
+        book.setAuthors(authors);
+        book.setIsbn("1234567890");
+        book.setName("Book 1");
+        book.setPublishers(pubs);
+        bookRepo.save(book);
+        
+        book = new Book();
+        authors.add("Auth 2");
+        pubs.add("Pub 2");
+        book.setAuthors(authors);
+        book.setIsbn("9876543210");
+        book.setName("Book 2");
+        book.setPublishers(pubs);
+        bookRepo.save(book);
+    }
+}

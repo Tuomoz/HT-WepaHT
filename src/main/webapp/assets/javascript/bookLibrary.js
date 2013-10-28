7dcefc1c041ed608b29e3ee025952d4fd8a9c3aa
Tuomo Kärkkäinen <tuomon.email@gmail.com>
2013-10-28 22:23:03 +0200

    Functionality to add multiple authors and publishers

--- src/main/webapp/assets/javascript/bookLibrary.js
+++ src/main/webapp/assets/javascript/bookLibrary.js
@@ -1,37 +1,55 @@
-var app = angular.module("BookLibrary", []);
+"use strict";
+var app = angular.module("BookLibrary", ["ngRoute"]);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/books', {
         templateUrl: 'templates/bookList.html',
         controller: BookListCtrl
     });
-    
+
     $routeProvider.when('/newbook', {
         templateUrl: 'templates/newbook.html',
         controller: NewBookCtrl
     });
-    
-    $routeProvider.otherwise({redirectTo: '/books'});
-}]);    
 
-app.filter("arrayToString", function() {
-    return function(input) {
+    $routeProvider.otherwise({redirectTo: '/books'});
+}]);
+
+app.filter("arrayToString", function () {
+    return function (input) {
         return input.join(", ");
-    }
+    };
 });
 
 function BookListCtrl($scope, $http) {
     $scope.testMsg = "working! ASDASDASD";
-    
-    $scope.fetchBooks = function() {
-        $http.get("app/books/books.json").success(function(list) {
+
+    $scope.fetchBooks = function () {
+        $http.get("app/books/books.json").success(function (list) {
             $scope.books = list;
+            $scope.books[0].authors.push("asd");
         });
     };
-    
+
     $scope.fetchBooks();
 }
 
 function NewBookCtrl($scope, $http) {
+    $scope.newBook = {name: "", isbn: "", authors: [""], publishers: [""]};
 
+    $scope.removeAuthor = function (index) {
+        $scope.newBook.authors.splice(index, 1);
+    };
+
+    $scope.addAuthor = function () {
+        $scope.newBook.authors.push("");
+    };
+
+    $scope.removePublisher = function (index) {
+        $scope.newBook.publishers.splice(index, 1);
+    };
+
+    $scope.addPublisher = function () {
+        $scope.newBook.publishers.push("");
+    };
 }
